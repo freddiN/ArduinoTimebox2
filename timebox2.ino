5505ce32ad0a6e706cf2a83e9df5f90bcb74c394
Freddi <muff99@outlook.com>
2021-04-25 02:23:13 +0200

    cleanup
    - even more F() usage to help with RAM usage
    - fix datetime formating
    - make time global, updating every 10 seconds is good enough for us

--- timebox2.ino
+++ timebox2.ino
@@ -21,10 +21,11 @@
 #define INTERVAL_CHECK_TIME 10000         //10 seconds in ms
 #define INTERVAL_DOWNLOAD_JSON 36000000   //10 hours in ms
 
-unsigned long time_now_a = 0;
-unsigned long time_now_b = 0;
+unsigned long time_now_a = 0, time_now_b = 0;
 boolean isInMeeting = false;
 String strJson = "";
+time_t now;
+tm tm;  
 
 ESP8266WebServer server(80);
 
@@ -37,12 +38,15 @@ void setup() {
 
   connectToWifi();
 
-  server.on("/", handleRoot);
+  server.on(F("/"), handleRoot);
   server.begin();
 
   configTime(MY_TZ, MY_NTP_SERVER);
 
   updateJson();   //get the JSON initially
+
+  time(&now);
+  localtime_r(&now, &tm);
 }
 
 void loop() {
@@ -60,14 +64,12 @@ void loop() {
 }
 
 void checkTimes() {  
+  time(&now);
+  localtime_r(&now, &tm); //update the global times, good enough to do this every 10 seconds
+  
   if (strJson.length() > 10) {
     DynamicJsonDocument doc(2048);
     deserializeJson(doc, strJson);
-
-    time_t now;
-    tm tm;  
-    time(&now);
-    localtime_r(&now, &tm);
 
     const int arraySize = doc["events"].size();
     for (int i = 0; i< arraySize; i++){
@@ -76,10 +78,10 @@ void checkTimes() {
       const char* end_date = doc["events"][i]["end_date"];
       const char* end_time = doc["events"][i]["end_time"];
 
-      if (!isInMeeting && timeMatches(tm, start_time) && dateMatches(tm, start_date)) {
+      if (!isInMeeting && timeMatches(start_time) && dateMatches(start_date)) {
         performServo(SERVO_ANGLE_OPEN);
         isInMeeting = true;
-      } else if (isInMeeting && timeMatches(tm, end_time) && dateMatches(tm, end_date)) {
+      } else if (isInMeeting && timeMatches(end_time) && dateMatches(end_date)) {
         performServo(SERVO_ANGLE_CLOSE);
         isInMeeting = false;
       }
@@ -90,7 +92,7 @@ void checkTimes() {
 /** 
  * strTime format 22:29
  */
-bool timeMatches(const tm tm, const String strTime) {
+bool timeMatches(const String& strTime) {
   const String strHour   = getValue(strTime, ':', 0);
   const String strMinute = getValue(strTime, ':', 1);
 
@@ -100,12 +102,12 @@ bool timeMatches(const tm tm, const String strTime) {
 /**
  * strDate format 23.04.2021 or *, but never on weekends!
  */
-bool dateMatches(const tm tm, const String strDate) {
+bool dateMatches(const String& strDate) {
   if(tm.tm_wday == 0 || tm.tm_wday == 6) {
     return false;
   }
   
-  if (strDate == "*") {
+  if (strDate == F("*")) {
     return true;
   }
 
@@ -116,13 +118,13 @@ bool dateMatches(const tm tm, const String strDate) {
   return (strDay.toInt() == tm.tm_hour && strMonth.toInt() == (tm.tm_mon + 1) && strYear.toInt() == (tm.tm_year + 1900));
 }
 
-void performServo(const int pos) {
+void performServo(const int& pos) {
   for (int i=0; i<40; i++) {
     setServo(pos);
   }
 }
 
-void setServo(const int pos) {
+void setServo(const int& pos) {
   const int puls = map(pos,0,180,400,2400);
   digitalWrite(PIN_SERVO, HIGH);
   delayMicroseconds(puls);
@@ -131,57 +133,50 @@ void setServo(const int pos) {
 }
 
 void handleRoot() {
-  String strAngle = "-1";
+  String strAngle = F("-1");
 
   if (server.arg("angle") != ""){
     strAngle = server.arg("angle");
   }
 
-  String HTML = "<!DOCTYPE html>\n";
-  HTML += "<html>\n";
-  HTML += "<head><title>Timebox</title></head>\n";
-  HTML += "<body>\n";
-  HTML += "<h2>Timebox</h2>\n";
+  String HTML;
+  HTML.reserve(2048);
+  HTML = F("<!DOCTYPE html>\n");
+  HTML += F("<html>\n");
+  HTML += F("<head><title>Timebox</title></head>\n");
+  HTML += F("<body>\n");
+  HTML += F("<h2>Timebox</h2>\n<p>\nip: ");
 
-  HTML += "<p>";
-  HTML += "ip: ";
   HTML += WiFi.localIP().toString();
-  HTML += "<br>\n";
+  HTML += F("<br>\nWIFI signal: ");
   
-  HTML += "WIFI signal: ";
   HTML += WiFi.RSSI();
-  HTML += "<br>\n";
-
-  HTML += "angle: ";
+  HTML += F("<br>\nangle: ");
   HTML += strAngle;
-  HTML += "<br>\n";
-  HTML += "timestamp: ";
-  HTML += getCurrentTime();
-  HTML += "<br>\n";
-  HTML += "inMeeting: ";
+  HTML += F("<br>\ntimestamp: ");
+
+  char buffer[18];
+  sprintf(buffer, "%02d.%02d.%d %02d:%02d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_hour, tm.tm_min); // 25.04.2021 01:45
+
+  HTML += buffer;
+  HTML += F("<br>\nin meeting: ");
   HTML += isInMeeting;
-  HTML += "<br>\n";
-  HTML += "JSON: ";
+  HTML += F("<br>\nJSON: ");
   HTML += strJson;
-  HTML += "<br>\n";
+  HTML += F("<br>\n<br>\n</p>\n");
 
-  HTML += "<br>\n";
+  HTML += generateForm(F("0"), F("0"));
+  HTML += generateForm(F("45"), F("45"));
+  HTML += generateForm(F("90"), F("90"));
+  HTML += generateForm(F("135"), F("135"));
+  HTML += generateForm(F("180"), F("180"));
+  HTML += generateForm(F("1000"), F("Up"));
+  HTML += generateForm(F("2000"), F("Down"));
+  HTML += generateForm(F("3000"), F("Update JSON"));
 
-  HTML += "</p>\n";
+  HTML += F("</body>\n</html>\n");
 
-  HTML += generateForm("0", "0");
-  HTML += generateForm("45", "45");
-  HTML += generateForm("90", "90");
-  HTML += generateForm("135", "135");
-  HTML += generateForm("180", "180");
-  HTML += generateForm("1000", "Up");
-  HTML += generateForm("2000", "Down");
-  HTML += generateForm("3000", "Update JSON");
-
-  HTML += "</body>\n";
-  HTML += "</html>\n";
-
-  server.send(200, "text/html", HTML);
+  server.send(200, F("text/html"), HTML);
 
   if (strAngle.toInt() >= 0 && strAngle.toInt() <= 180) {
     performServo(strAngle.toInt());
@@ -194,11 +189,17 @@ void handleRoot() {
   }
 }
 
-String generateForm(const String strAngle, const String strText) {
-  String HTML = "<form action=\"/\" method=\"GET\">\n";
-  HTML += "  <input type=\"hidden\" name=\"angle\" value=\"" + strAngle + "\"/>\n";
-  HTML += "  <input type=\"submit\" value=\"" + strText +"\">\n";
-  HTML += "</form>\n";
+String generateForm(const String& strAngle, const String& strText) {
+  String HTML;
+  HTML.reserve(160);
+  HTML = F("<form action=\"/\" method=\"GET\">\n");
+  HTML += F("  <input type=\"hidden\" name=\"angle\" value=\""); 
+  HTML += strAngle;
+  HTML += F("\"/>\n");
+  HTML += F("  <input type=\"submit\" value=\"");
+  HTML += strText;
+  HTML += F("\">\n");
+  HTML += F("</form>\n");
   return HTML;
 }
 
@@ -221,7 +222,7 @@ void connectToWifi() {
     Serial.println(F("Connected!")); 
     Serial.print(F("ip address: ")); 
     Serial.println(WiFi.localIP());
-    long rssi = WiFi.RSSI();
+    const long rssi = WiFi.RSSI();
     Serial.print(F("RSSI:"));
     Serial.println(rssi);
   } else {
@@ -240,64 +241,13 @@ void updateJson() {
   http.end();
 }
 
-String getCurrentTime() {
-  time_t now;
-  tm tm;  
-  time(&now);                       // read the current time
-  localtime_r(&now, &tm);           // update the structure tm with the current time
-  
-  char buffer [50];
-  
-  String TXT = "";
-
-  if (tm.tm_mday < 10) {
-    TXT.concat("0");
-  }
-  itoa(tm.tm_mday, buffer, 10);
-  TXT.concat(buffer);
-  TXT.concat(".");
-
-  if (tm.tm_mon + 1 < 10) {
-    TXT.concat("0");
-  }
-  itoa(tm.tm_mon + 1, buffer, 10);
-  TXT.concat(buffer);
-  TXT.concat(".");
-
-  itoa(tm.tm_year + 1900, buffer, 10);
-  TXT.concat(buffer);
-  TXT.concat(" ");
-
-  if (tm.tm_hour < 10) {
-    TXT.concat("0");
-  }
-  itoa(tm.tm_hour, buffer, 10);
-  TXT.concat(buffer);
-  TXT.concat(":");
-
-  if (tm.tm_min < 10) {
-    TXT.concat("0");
-  }
-  itoa(tm.tm_min, buffer, 10);
-  TXT.concat(buffer);
-  TXT.concat(":");
-
-  if (tm.tm_sec < 10) {
-    TXT.concat("0");
-  }
-  itoa(tm.tm_sec, buffer, 10);
-  TXT.concat(buffer);
-
-  return TXT;
-}
-
 /**
  * String xval = getValue(myString, ':', 0);
  */
-String getValue(const String data, const char separator, const int index){
+String getValue(const String& data, const char& separator, const int& index){
   int found = 0;
   int strIndex[] = { 0, -1 };
-  int maxIndex = data.length() - 1;
+  const int maxIndex = data.length() - 1;
 
   for (int i = 0; i <= maxIndex && found <= index; i++) {
     if (data.charAt(i) == separator || i == maxIndex) {
